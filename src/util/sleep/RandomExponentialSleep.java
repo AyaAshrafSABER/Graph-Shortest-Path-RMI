6ccd65c6b772c2f4c2bfbd80296d1c9a115b8536
Khaled Barie <k.alkiek@gmail.com>
2020-03-31 21:03:43 +0200

    Fix random exponential sleep

--- src/util/sleep/RandomExponentialSleep.java
+++ src/util/sleep/RandomExponentialSleep.java
@@ -1,22 +1,26 @@
 package util.sleep;
 
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
 public class RandomExponentialSleep implements RandomSleep {
     private double lambda;
     private long min, max;
+    Random rand;
 
     public RandomExponentialSleep(long mean) {
         this.lambda = 1.0/mean;
         this.min = 0;
         this.max = Long.MAX_VALUE;
+        rand = new Random();
     }
 
     public RandomExponentialSleep(long mean, long min, long max) {
         this.lambda = 1.0/mean;
         this.min = min;
         this.max = max;
+        rand = new Random();
     }
 
     public void sleep() throws InterruptedException {
@@ -31,7 +35,7 @@ public class RandomExponentialSleep implements RandomSleep {
     }
 
     private long generateRandom() {
-        long r = (long)(lambda * Math.log(1-Math.random())/(-lambda));
+        long r = (long)((Math.log(1-rand.nextDouble())/(-lambda)));
         if (r < min) r = min;
         else if (r > max) r= max;
         return r;
